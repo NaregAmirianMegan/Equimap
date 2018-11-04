fa25c4438a3eaff11ca816329a889f702b9add7a
Nareg Megan <nareg@airbears2-10-142-2-7.airbears2.1918.berkeley.edu>
2018-11-04 02:39:00 -0800

    update

--- gather_urls.py
+++ gather_urls.py
@@ -1,26 +1,3 @@
-# from bs4 import BeautifulSoup
-# from bs4.element import Comment
-# import requests
-#
-# url_base = 'https://www.google.com/search?q='
-#
-# def get_google_search(input):
-#     rest = ''
-#     for string in input.split(' '):
-#         rest += string
-#         rest += '+'
-#     return url_base + rest[:-1]
-#
-# # def parse_html_for_hrefs(html):
-#
-# def get_urls(input):
-#     url = get_google_search(input)
-#     html = requests.get(url).text
-#     soup = BeautifulSoup(html, 'html.parser')
-#     print(soup.find_all('cite'))
-#
-# get_urls('orange peels')
-
 from googlesearch import search
 
 def get_urls(input, num_results=10):
--- input_to_corpus.py
+++ input_to_corpus.py
@@ -1,9 +1,15 @@
 from bs4 import BeautifulSoup
 from bs4.element import Comment
-import requests
-import execnet
+import requests, execnet
 
-# from gather_urls import get_urls
+url_base = 'https://www.google.com/search?q='
+
+def get_google_url(input):
+    rest = ''
+    for string in input.split(' '):
+        rest += string
+        rest += '+'
+    return url_base + rest[:-1]
 
 def call_python_version(Version, Module, Function, ArgumentList):
     gw      = execnet.makegateway("popen//python=python%s" % Version)
@@ -22,12 +28,19 @@ def tag_visible(element):
     return True
 
 def text_from_html(body):
-    soup = BeautifulSoup(body, 'html.parser')
+    soup = BeautifulSoup(body, 'lxml')
     texts = soup.findAll(text=True)
     visible_texts = filter(tag_visible, texts)
     return u" ".join(t.strip() for t in visible_texts)
 
+# def get_lat_long(input):
+#     google_url = get_google_url(input)
+#     soup = BeautifulSoup(requests.get(google_url).text, 'lxml')
+#     hq = soup.find(text='Headquarters').a.next_sibling
+#     return hq
+
 def compile_corpus(input, num_results=10):
+    # lat, long = get_lat_long(input)
     urls = call_python_version("2.7", "gather_urls", "get_urls",
                                  [input, num_results])
     text_list = []
@@ -36,4 +49,4 @@ def compile_corpus(input, num_results=10):
         text_list.append(text_from_html(html))
     return text_list
 
-print(compile_corpus('tesla', 2))
+print(compile_corpus('AAPL', 2))
